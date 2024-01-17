204b87fe45413a2b7fbdaf8de13684ac7a0d30e6
Singgih Pratama <singgih.vendor.pasti@pegadaian.co.id>
2024-01-17 22:46:06 +0700

    initialize project konselor siswa server

---
+++ src/main/java/tech/tok/konselorsiswaserver/KonselorSiswaServerApplication.java
@@ -0,0 +1,13 @@
+package tech.tok.konselorsiswaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonselorSiswaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonselorSiswaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/tok/konselorsiswaserver/KonselorSiswaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.tok.konselorsiswaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonselorSiswaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
